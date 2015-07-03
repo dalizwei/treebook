6733f1a35f7f04ed25ff0c56096e269e871d3e39
mohamed ali <mohamed.ali.sfari@gmail.com>
2015-07-03 16:28:55 +0100

    generate profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Treebook::Application.routes.draw do
+  get "profiles/show"
   devise_for :users, controllers: { confirmations: "users/confirmations",
                                     passwords: "users/passwords",
                                     registrations: 'users/registrations',
