40dea4195a0cf549ef354059ab6f34ecd545638b
mohamed ali <mohamed.ali.sfari@gmail.com>
2015-07-07 08:55:44 +0100

    create status without selecting a user

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -26,7 +26,7 @@ class StatusesController < ApplicationController
   # POST /statuses.json
   def create
     @status = Status.new(status_params)
-
+    @status.user_id = current_user.id
     respond_to do |format|
       if @status.save
         format.html { redirect_to @status, notice: 'Status was successfully created.' }
